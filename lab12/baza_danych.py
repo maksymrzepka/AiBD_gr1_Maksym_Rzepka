1e21e863a95c0c87884698ec02caeff588645549
maksymrzepka <92428393+maksymrzepka@users.noreply.github.com>
2023-01-06 13:20:25 +0100

    Add files via upload

---
+++ lab12/baza_danych.py
@@ -0,0 +1,24 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+
+from sqlalchemy import Column, Integer, String, Date
+
+
+db_string = "postgresql://postgres:37wok2mesB!@localhost:5432/lab12"
+
+engine = create_engine(db_string)
+
+Base = declarative_base()
+
+class Author(Base):
+    __tablename__ = 'authors'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50))
+    surname = Column(String(50))
+    born_date = Column(Date)
+
+    def __repr__(self):
+        return "<authors(id='{0}', name={1}, surname={2}, born_date={3})>".format(
+            self.id, self.name, self.surnamey, self.born_date)
+
+Base.metadata.create_all(engine)
